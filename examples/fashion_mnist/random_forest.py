fe7294f63d2f3c4e71c4321ac24ee82bc221befb
Matthieu <matth.le.goff@free.fr>
2019-03-01 15:11:12 +0100

    Missing files from last commit

--- examples/fashion_mnist/random_forest.py
+++ examples/fashion_mnist/random_forest.py
@@ -1,16 +1,13 @@
-import mlflow
 from sklearn.ensemble import RandomForestClassifier
 import logging
-
-from sklearn.pipeline import make_pipeline
-
 from examples.fashion_mnist.data import FashionMnistLoader
+from nautilus.dataset import dataset_utils
 from nautilus.experiment.experiment import Experiment
-from nautilus.metrics.buffer_accuracy import BufferAccuracy
 from nautilus.metrics.classification_report import BufferedClassificationReport
 from nautilus.metrics.confusion_matrix import BufferedConfusionMatrix
-from nautilus.transformer.from_feature import TransformerFromFeature
+from nautilus.transform.sequential import Sequential
 from nautilus.utils import image_utils, np_utils
+
 from absl import app
 from absl import flags
 
@@ -22,40 +19,30 @@ flags.DEFINE_integer("n_estimators", 25, "Number of decision trees to learn")
 
 logging.basicConfig(level=logging.INFO)
 
-
 def main(_):
     dataset = FashionMnistLoader().dataset()
+
+    feature_computer = Sequential.from_transforms(
+        image_utils.cvresize_fn((FLAGS.shape_resized, FLAGS.shape_resized)),
+        np_utils.flatten
+    )
 
     # Random forest
 
     rf = RandomForestClassifier(max_depth=None, n_estimators=FLAGS.n_estimators,
                                 bootstrap=True, n_jobs=FLAGS.n_jobs)
 
-    pipeline = make_pipeline(
-        TransformerFromFeature(image_utils.cvresize_fn((FLAGS.shape_resized,
-                                                        FLAGS.shape_resized))),
-        TransformerFromFeature(np_utils.flatten),
-        rf
+    experiment = Experiment(
+        train_dataset_fn=lambda : dataset_utils.map_x(dataset.train,
+                                                      feature_computer),
+        test_dataset_fn=lambda : dataset_utils.map_x(dataset.test,
+                                                      feature_computer),
+        model=rf,
+        exp_tag="rf_exp",
+        metrics=[BufferedConfusionMatrix(), BufferedClassificationReport()]
     )
 
-    with mlflow.start_run():
-
-        mlflow.log_artifact(__file__)
-
-        mlflow.log_param("n_estimators", FLAGS.n_estimators)
-        mlflow.log_param("shape_resized", FLAGS.shape_resized)
-
-        experiment = Experiment(
-            train_dataset_fn=lambda : dataset.train,
-            test_dataset_fn=lambda : dataset.test,
-            model=pipeline,
-            exp_tag="rf_exp",
-            metrics=[BufferedConfusionMatrix(), BufferedClassificationReport(),
-                     BufferAccuracy()],
-            use_cache=False
-        )
-
-        experiment.run()
+    experiment.run()
 
 if __name__ == '__main__':
     app.run(main)
